92bfe651c291117cba91da32b6c756c456fd6de6
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-12-22 21:34:27 -0300

    incluindo horario de funcionamento no footer

--- js/script.js
+++ js/script.js
@@ -21,3 +21,17 @@ new SimpleSlide({
 if (window.SimpleAnime){
     new SimpleAnime();
 }
+
+const funcionamento = document.querySelector('[data-semana]')
+const diasSemana = funcionamento.dataset.semana.split(',').map(Number)
+const horaSemana = funcionamento.dataset.hora.split(',').map(Number)
+
+const horaAhora= new Date().getHours()
+const diaAgora = new Date().getDay()
+
+const diaAberto = diasSemana.indexOf(diaAgora) !== -1;
+const horaAberta = horaAhora >= horaSemana[0] && horaAhora < horaSemana[1]
+
+if(diaAberto && horaAberta) {
+    funcionamento.classList.add('aberto')
+}
